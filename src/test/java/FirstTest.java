66411cdc285eebc30b45c92fe70e03552a730022
Maksim <maks_art@yahoo.com>
2025-04-02 16:38:15 +0300

    today

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -7,38 +7,45 @@ import java.io.IOException;
 
 public class FirstTest {
     private WebDriver driver;
-    IndexPage page;
+    private SignInPage page;
 
     @BeforeAll
-    public static void SetupDriver(){
+    public static void SetupDriver() {
         WebDriverManager.firefoxdriver().setup();
     }
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         driver = new FirefoxDriver();
-        page = new IndexPage(driver);
+        page = new SignInPage(driver);
         page.open();
     }
 
     @AfterEach
-    public void close(){
+    public void close() {
         driver.close();
     }
 
     @Test
-    public void Test1() {
+    public void test1() {
         String expected = "Введите номер телефона";
-        String actual = page.clickButton().getElement();
-        Assertions.assertEquals(expected,actual);
+        String actual = page.goToRegistrationPage().getHeader();
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void Test2() throws IOException {
+    public void test2() throws IOException {
         String expected = "https://ok.ru/dk?st.cmd=anonym2FAPhoneConfirm";
         String actual = page.authorize(new ConfProperties().getDetails("login"),
-                new ConfProperties().getDetails("password")).getPageUrl();
-        Assertions.assertEquals(expected,actual);
+                new ConfProperties().getDetails("password")).getCurrentPageUrl();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void test3() {
+        String expected = "technopol37 technopol37";
+        String actual = page.authorize("technopol37", "technopolisPassword").checkHeader();
+        Assertions.assertEquals(expected, actual);
     }
 }
 
--- src/test/java/RegistrationPage.java
+++ src/test/java/RegistrationPage.java
@@ -1,15 +1,17 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import pageObject.BasePage;
 
 public class RegistrationPage extends BasePage {
+
+    private By header = By.cssSelector(".ext-registration_h");
+
     public RegistrationPage(WebDriver driver) {
         super(driver);
     }
 
-    public String getElement(){
-        WebElement element = getDriver().findElement(By.cssSelector(".ext-registration_h"));
-        return element.getText();
+    public String getHeader(){
+        return readText(header);
     }
+
 }
